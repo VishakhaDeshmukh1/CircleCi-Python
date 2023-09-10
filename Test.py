b06af9d02b716e3ea3e6b00a3d6379132f96640f
VishakhaDeshmukh1 <vishakhadeshmukh116@gmail.com>
2023-09-10 15:53:15 +0530

    added python file

--- Test.py
+++ Test.py
@@ -2,9 +2,9 @@ import unittest
 from main import to_upper
 class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
-        name = "Rida"
+        name = "Vishakha"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "RIDA")
+        self.assertEqual(upper_name, "Vishakha")
 
     if __name__ == '__main__':
        unittest.main()  
