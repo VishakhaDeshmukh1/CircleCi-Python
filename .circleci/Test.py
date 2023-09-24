5080c4981198e05ff7d0995fd2a860c3138209fb
VishakhaDeshmukh1 <vishakhadeshmukh116@gmail.com>
2023-09-24 15:40:36 +0530

    Added files

---
+++ .circleci/Test.py
@@ -0,0 +1,10 @@
+import unittest
+from main import to_upper
+class MyTestCase(unittest.TestCase):
+    def test_to_upper(self):
+        name = "Vishakha"
+        upper_name = to_upper(name)
+        self.assertEqual(upper_name, "Vishakha")
+
+    if __name__ == '__main__':
+       unittest.main()  
